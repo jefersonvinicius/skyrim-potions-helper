f44e11fc9d9991cf44bef2cea7b2617eef53cbcf
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2022-08-31 21:37:12 -0300

    Adding typescript and webpack

--- main.js
+++
@@ -1,9 +0,0 @@
-const { selectPotions } = require('./dist/select-potions');
-
-async function main() {
-  const ingredients = ['Blue Dartwing', 'Nordic Barnacle', 'Abecean Longfin'];
-  const result = selectPotions({ ingredients });
-  console.table(result);
-}
-
-main();
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './app/index.ts',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'app/client'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+};
